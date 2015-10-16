80eb8574e9489040fb2b46954b058d9e20e160d5
andymememe <andymememe@gmail.com>
2015-10-16 22:14:13 +0800

    Complete login api

--- src/login/login.php
+++ src/login/login.php
@@ -1,43 +1,51 @@
 <?php
+	header("Content-Type:text/html; charset=utf-8");
+	
 	$user = $_POST["account_id"];
 	$password = $_POST["password"];
-	$now = getdata();
-	
+	$now = getdate();
+
 	/* Database Setting */
-	$dburl = "";
-	$dbuser = "";
-	$dbpass = "";
+	$dburl = "luthertsai.com";
+	$dbuser = " ";
+	$dbpass = " ";
 	$db = "2015softwareengineering";
-	
+
 	$sqli = @new mysqli($dburl, $dbuser, $dbpass, $db);
 	$errno = mysqli_connect_errno();
 	if($errno)
 	{
 		$feedback = array('success' => 0, 'message' => 'db_error');
-		echo json_encode($feedback);
+		echo(json_encode($feedback));
 		exit();
 	}
-	$stmt = $sqli->prepare("SELECT uid, name FROM user_info WHERE account_id = ? AND password = ?;");
-	$stmt->bind_param("ss", $user, md5($password));
-	$stmt->execute();
+
+	$sqli->query("SET NAMES 'UTF8'");
+	$result = $sqli->query("SELECT COUNT(uid) AS c, uid, name, previlege FROM user_info WHERE account_id='" . $user . "' AND password='" . md5($password) . "';") or die('Query error');
 	
-	$result = $stmt->get_result();
-	if($result->num_rows == 1)
+	while($row = $result->fetch_array(MYSQLI_ASSOC))
 	{
-		$row = $result->fetch_array(MYSQLI_ASSOC);
-		
-		session_start();
-		$_SESSION['sessionid'] = md5("{$row['uid']}{$now['year']}{$now['mon']}{$now['mday']}{$now['hours']}{$now['minutes']}{$now['seconds']}");
-		session_write_close();
-		
-		$feedback = array('success' => 1, 'message' => 'ok', 'uid' => $row['uid'], 'name' => $row['name'], 'sid' = "{$row['uid']}{$now['year']}{$now['mon']}{$now['mday']}{$now['hours']}{$now['minutes']}{$now['seconds']}");
-		echo json_encode($feedback);
-	}
-	else
-	{
-		$feedback = array('success' => 0, 'message' => 'result_error');
-		echo json_encode($feedback);
-	}
-	
-	$sqli->close();
-?>
+        if($row['c'] == 1)
+		{
+			session_start();
+			$_SESSION['sessionid'] = md5("{$row['uid']}{$now['year']}{$now['mon']}{$now['mday']}{$now['hours']}{$now['minutes']}{$now['seconds']}");
+			session_write_close();
+			
+			
+			$feedback = array();
+			$feedback['success'] = '1';
+			$feedback['message'] = 'ok';
+			$feedback['uid'] = $row['uid'];
+			$feedback['name'] = urlencode($row['name']);
+			$feedback['previlege'] = $row['previlege'];
+			
+			echo(urldecode(json_encode($feedback)));
+		}
+		else
+		{
+			$feedback = array('success' => 0, 'message' => 'result_error');
+			echo(json_encode($feedback));
+			$sqli->close();
+			exit();
+		}
+    }
