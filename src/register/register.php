8990314b836518d40f9bdd1622bdd6d502a8f540
Luther Tsai <Luther0429@gmail.com>
2015-10-23 15:25:21 +0800

    Add Register API

---
+++ src/register/register.php
@@ -0,0 +1,39 @@
+﻿<?php
+
+header("Content-Type:text/html; charset=utf-8");
+
+$aName = $_POST["Name"];
+$aAccount = $_POST["AccountID"];
+$aPassword = md5($_POST["Password"]);
+$aEmail = $_POST["Email"];
+$aCompany = $_POST["Company"];
+$aPrevilege = intval($_POST["Previlege"]);
+
+/* Database Setting */
+$dburl = "";
+$dbuser = "";
+$dbpass = "";
+$db = "2015softwareengineering";
+	
+// Create connection
+$conn = mysqli_connect($dburl, $dbuser, $dbpass, $db);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+//Show Chinese Chracters Correctly
+mysqli_query($conn, "SET NAMES 'UTF8'");
+
+//Insert Course Criticize to SQL
+$insert = "INSERT INTO user_info (`uid`, `name`, `account_id`, `password`, `email`, `company`, `previlege`, `user_session`) VALUES (NULL, '$aName', '$aAccount', '$aPassword', '$aEmail', '$aCompany', '$aPrevilege', NULL)";
+
+if (mysqli_query($conn, $insert)) {
+	$feedback = array();
+	$feedback['success'] = '1';
+	echo(json_encode($feedback));
+} else {
+	echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+?>
