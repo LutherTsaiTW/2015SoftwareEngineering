bde57a088be6807d7dc8ec861ed4c2c8b9f60eae
kevin <kevin15121@gmail.com>
2015-11-01 18:49:24 +0800

    projectlist page

---
+++ src/projectlist/showProjectList.js
@@ -0,0 +1,33 @@
+
+var xmlhttp = new XMLHttpRequest();
+var url = "projectlist.php";
+
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        myFunction(xmlhttp.responseText);
+    }
+}
+
+
+function myFunction(response) {
+    var arr = JSON.parse(response);
+	var project=arr.projects;
+    var i;
+    var out = "<table class=\"listTable\">";
+
+    for(i = 0; i < project.length; i++) {
+        out += "<tr><td>" + 
+        project[i].name +
+        "</td><td>" +
+        project[i].des +
+        "</td><td>" +
+        "edit" +
+        "</td></tr>";
+    }
+    out += "</table>";
+	document.getElementById("userName").innerHTML ="Welcome, "+arr.name+"!";
+    document.getElementById("listTable").innerHTML =out;
+}
