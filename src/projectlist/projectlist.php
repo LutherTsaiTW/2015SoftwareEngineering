c5c8ad34c9f8c773a0eae2407e5829684f4dda6f
andymememe <andymememe@gmail.com>
2015-11-03 21:35:39 +0800

    Project List API Fixed

--- src/projectlist/projectlist.php
+++ src/projectlist/projectlist.php
@@ -1,51 +1,14 @@
 <?php
-	/* [AC] Database Setting */
-	$dburl = "";
-	$dbuser = "";
-	$dbpass = "";
-	$db = "2015softwareengineering";
-
-	$user_session = null;
-	session_start();
-	$user_session = $_SESSION['sessionid']; // [CLY] Get Session ID
-	session_write_close();
-	
-	$sqli = @new mysqli($dburl, $dbuser, $dbpass, $db);
-	$errno = mysqli_connect_errno();
-	if($errno)
-	{
-		$feedback = array('success' => 0, 'message' => 'db_error');
-		echo(json_encode($feedback));
-		exit();
-	}
-	
-	$sqli->query("SET NAMES 'UTF8'");
-	$result = $sqli->query("SELECT COUNT(uid) AS c, uid, name, previlege FROM user_info WHERE user_session='" . $user_session . "';") or die('Session query error'); // [CLY] Get user info
-	
-	$feedback = array();
-	$uid = null;
-	while($row = $result->fetch_array(MYSQLI_ASSOC))
-	{
-        if($row['c'] == 1)
-		{
-			$uid = $row['uid'];
-			
-			$feedback['name'] = urlencode($row['name']); // [CLY] urlencode : Let Chinese charcters show correctly
-			$feedback['previlege'] = $row['previlege'];
-		}
-		else
-		{
-			$feedback = array('success' => 0, 'message' => 'session_error');
-			echo(json_encode($feedback));
-			$sqli->close();
-			exit();
-		}
-    }
+	require_once('assist/GetUserInfo.php');
     
-    /* [AC] Get project list */
-    $result = $sqli->query("SELECT p.p_id, p.p_name, p.p_des, p.p_company, p.p_owner, p.p_start_time, p.p_end_time FROM project AS p LEFT JOIN project_team AS t ON p.p_id = t.project_id WHERE t.user_id = " . $uid . ";") or die('Project query error');
+    /* [CLY] Get project list */
+    $result = $sqli->query("SELECT p.p_id, p.p_name, p.p_des, p.p_company, p.p_owner, p.p_start_time, p.p_end_time FROM project AS p LEFT JOIN project_team AS t ON p.p_id = t.project_id WHERE t.user_id = " . $user['uid'] . ";") or die('Project query error');
     
-	$feedback['projects'] = array();
+	$feedback = $user;
+	$feedback['name'] = urlencode($user['name']);
+	$feedback['company'] = urlencode($user['company']);
+	unset($feedback['password']);
+	unset($feedback['user_session']);
 	while($row = $result->fetch_array(MYSQLI_ASSOC))
 	{
 		$pid = $row['p_id'];
