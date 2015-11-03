11068c759f38e7b964901ebaccd9f1dd1f10e826
andymememe <andymememe@gmail.com>
2015-11-03 23:35:36 +0800

    Return Owner Name

--- src/projectlist/projectlist.php
+++ src/projectlist/projectlist.php
@@ -11,11 +11,13 @@
 	unset($feedback['user_session']);
 	while($row = $result->fetch_array(MYSQLI_ASSOC))
 	{
+		$result_owner = $sqli->query("SELECT name FROM user_info WHERE uid = " . $row['p_owner'] . ";");
+		$row_owner = $result_owner->fetch_array(MYSQLI_ASSOC);
 		$pid = $row['p_id'];
 		$feedback['projects'][$pid]['name'] = urlencode($row['p_name']);
 		$feedback['projects'][$pid]['des'] = urlencode($row['p_des']);
 		$feedback['projects'][$pid]['company'] = urlencode($row['p_company']);
-		$feedback['projects'][$pid]['owner'] = urlencode($row['p_owner']);
+		$feedback['projects'][$pid]['owner'] = urlencode($row_owner['name']);
 		$feedback['projects'][$pid]['start_time'] = urlencode($row['p_start_time']);
 		$feedback['projects'][$pid]['end_time'] = urlencode($row['p_end_time']);
 		$feedback['success'] = '1';
