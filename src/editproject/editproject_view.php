429d16c3249fff67cdb6e2e28ff3a01d5a7f9fc0
Liang-Yu Chen <andymememe@gmail.com>
2015-11-05 22:59:22 +0800

    Edit Project View finished

--- src/editproject/editproject_view.php
+++ src/editproject/editproject_view.php
@@ -21,20 +21,15 @@
 
 <body class="w3-container" style="background-color:rgb(61, 61, 61)">
     <?php $pid = $_GET['pid']; ?>
-    <br/>
+    <br>
     <div class="w3-row ">
         <div style="float:left">
-            <img src="../imgs/pts_icon.png" alt="ICON" width="100" Height="30" />
+            <img src="imgs/pts_icon.png" alt="ICON" width="100" Height="30" />
         </div>
-        <div class="w3-container fastAccount">
-            Login
-        </div>
-        <div class="fastAccountBlock">
-            <p/>
-        </div>
-        <div class="w3-container fastAccount">
-            Register
-        </div>
+        <div class="w3-container" id="userName" style="float:right;color:white;font-size:18">
+			Welcome!
+		</div>
+		<script type="text/javascript" src="js/getUser.js"></script>
     </div>
     <div class="w3-row " style="Height:30%;color:white;text-align:center">
         <h1 style="background-color:grey;border-radius:5px">
@@ -49,8 +44,82 @@
             <div class="w3-col m4">
                 <p></p>
             </div>
-            <form action="" method="POST">
-                <input type="hidden" name="pid" value="<?=$pid; ?>" />
+            <form action="" method="POST" id="editProject">
+                <br>
+                <div class="w3-row formBlock" style>
+                    <div class="w3-col m2">
+						<p></p>
+                    </div>
+                    <div class="w3-col m4" align="left">
+						<input id="pid" type="hidden" value="<?=$pid; ?>" />
+						<?php
+							/* Database Setting */
+							$dburl = "";
+							$dbuser = "";
+							$dbpass = "";
+							$db = "2015softwareengineering";
+							
+							// Create connection
+							$conn = mysqli_connect($dburl, $dbuser, $dbpass, $db);
+							// Check connection
+							if (!$conn)
+							{
+								die("Connection failed: " . mysqli_connect_error());
+							}
+
+							//Show Chinese Chracters Correctly
+							mysqli_query($conn, "SET NAMES 'UTF8'");
+							
+							//Insert Course Criticize to SQL
+							$select = "SELECT * FROM project WHERE p_id = " . $pid . ";";
+							$result = mysqli_query($conn, $select);
+							
+							if ($row = $result->fetch_array(MYSQLI_ASSOC))
+							{
+								$feedback = $row;
+								$feedback['success'] = '1';
+							}
+							else
+							{
+								echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+								exit;
+							}
+						?>
+                        <br>
+                        <font color="white">Name:</font>
+                        <br>
+                        <input id="name" type="text" name="name" required style="border-radius: 3px" placeholder="Enter a name" value="<?=$row["p_name"] ?>"/>
+                        <font color="white">Company:</font>
+                        <br>
+                        <input id="company" type="text" name="company" required style="border-radius: 3px" placeholder="Enter the company's name" value="<?=$row["p_company"] ?>"/>
+                        <br>
+                        <font color="white">Start Time:</font>
+                        <br>
+                        <input id="startTime" type="datetime" name="startTime" required style="border-radius: 3px" value="<?=$row["p_start_time"] ?>"/>
+                        <br>
+                        <font color="white">End Time:</font>
+                        <br>
+                        <input id="endTime" type="datetime" name="endTime" required style="border-radius: 3px" value="<?=$row["p_end_time"] ?>"/>
+                        <br>
+                        <font color="white">Status:</font>
+                        <br>
+                        <select name="status" id="status" required style="border-radius: 3px" form="editProject">
+						  <option value=1 <?php if($row["p_start_time"] == 1) echo("default"); ?>>進行中</option>
+						  <option value=2 <?php if($row["p_start_time"] == 2) echo("default");  ?>>完成</option>
+						  <option value=3 <?php if($row["p_start_time"] == 3) echo("default");  ?>>中止</option>
+						</select>
+                        <br>
+                        <font color="white">Description:</font>
+                        <br>
+                        <textarea rows="4" name="des" id="des" width="322px">
+							<?=$row["p_des"] ?>
+						</textarea> 
+                        <br>
+                        <input type="submit" name="submit" value="Add" class="w3-teal">
+                        <input type="button" name="exit" value="Exit" class="w3-teal" onclick="location.href='projectlist.html';">
+                        <br>
+                        <br>
+                </div>
             </form>
             <div class="w3-col m4">
                 <p></p>
