ace87170d8aa4d09bf199eeb18b7c38e6f415d16
andymememe <andymememe@gmail.com>
2015-11-13 12:44:11 +0800

    Add Member finished

---
+++ src/projectDetail/addMember.php
@@ -0,0 +1,28 @@
+<?php
+	$addusers = $_POST['addusers'];
+	$pid = $_POST['pid'];
+
+	/* [CLY] Database Setting */
+	$dburl = "luthertsai.com";
+	$dbuser = "";
+	$dbpass = "";
+	$db = "";
+
+	$sqli = @new mysqli($dburl, $dbuser, $dbpass, $db);
+	$errno = mysqli_connect_errno();
+	if($errno)
+	{
+		$feedback = array('success' => 0, 'message' => 'db_error');
+		echo(json_encode($feedback));
+		exit();
+	}
+	
+	$sqli->query("SET NAMES 'UTF8'"); // [CLY] Let Chinese charcters show correctly
+	foreach ($addusers as $adduser)
+	{
+		$sqli->query("INSERT INTO project_team (project_id, user_id) VALUES (" . $pid . ", " . $adduser . ");") or die('Query error');
+	}
+	
+	$feedback['success'] = 1;
+	echo json_encode($feedback);
+?>
