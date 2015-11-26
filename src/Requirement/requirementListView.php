12a242adda9200e5a3167ca77aabb53eae76973c
andymememe <andymememe@gmail.com>
2015-11-26 13:14:28 +0800

    Fixed title

--- src/Requirement/requirementListView.php
+++ src/Requirement/requirementListView.php
@@ -1,7 +1,61 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Edit Project</title>
+		<?php
+			$pid = $_GET['pid'];
+			
+			require_once '../assist/DBConfig.php';
+			$sqli = @new mysqli($dburl, $dbuser, $dbpass, $db);
+			if($sqli->connect_errno)
+			{
+				$feedback = array('success' => 0, 'message' => $sqli->connect_error);
+				echo(json_encode($feedback));
+				exit;
+			}
+			
+			//Show Chinese Chracters Correctly
+			$sqli->query("SET NAMES 'UTF8'");
+			
+			session_start();
+			$session = $_SESSION['sessionid'];
+			session_write_close();
+			
+			$result = $sqli->query("SELECT uid, name, previlege FROM user_info WHERE user_session='" . $session . "'") or die($sqli->error);
+			if (!($userinfo = $result->fetch_array(MYSQLI_ASSOC)))
+			{
+				$feedback = array('success' => 0, 'message' => 'userinfo_fetch_error');
+				echo(json_encode($feedback));
+				exit;
+			}
+			
+			$result = $sqli->query("SELECT p.p_id, p.p_name, p.p_des, p.p_company, u.name AS owner, p.p_start_time, p.p_end_time, p.status FROM project AS p LEFT JOIN user_info AS u ON p.p_owner = u.uid WHERE p_id=" . $pid . ";") or die($sqli->error);
+			if(!($project_info = $result->fetch_array(MYSQLI_ASSOC)))
+			{
+				$feedback = array('success' => 0, 'message' => 'project_fetch_error');
+				echo(json_encode($feedback));
+				exit;
+			}
+			
+			$result = $sqli->query("SELECT u.name FROM project_team AS p RIGHT JOIN user_info AS u ON u.uid = p.user_id WHERE p.project_id=" . $pid . ";") or die($sqli->error);
+			while($row = $result->fetch_array(MYSQLI_ASSOC))
+			{
+				$members[]['name'] = $row['name'];
+			}
+			
+			$reqs = array();
+			$result = $sqli->query("SELECT r.rid, r.rname, r.rtype, r.rdes, r.rstatus, r.rpriority, u.name AS owner FROM req AS r LEFT JOIN user_info AS u ON r.rowner = u.uid WHERE rproject=" . $pid . " AND rstatus != 0 ORDER BY rpriority DESC;") or die($sqli->error);
+			while($row = $result->fetch_array(MYSQLI_ASSOC))
+			{
+				$reqs[$row['rid']]['id'] = $row['rid'];
+				$reqs[$row['rid']]['name'] = $row['rname'];
+				$reqs[$row['rid']]['type'] = $row['rtype'];
+				$reqs[$row['rid']]['des'] = $row['rdes'];
+				$reqs[$row['rid']]['status'] = $row['rstatus'];
+				$reqs[$row['rid']]['priority'] = $row['rpriority'];
+				$reqs[$row['rid']]['owner'] = $row['owner'];
+			}
+		?>
+		<title><?= $project_info['p_name']; ?> Requirements</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="../css/w3.css">
 		<link rel="stylesheet" type="text/css" href="../css/html5tooltips.css" />
@@ -216,60 +270,6 @@
 	}
 	</style>
 	<body class="w3-container" style="background-color:rgb(61, 61, 61)">
-		<?php
-			$pid = $_GET['pid'];
-			
-			require_once '../assist/DBConfig.php';
-			$sqli = @new mysqli($dburl, $dbuser, $dbpass, $db);
-			if($sqli->connect_errno)
-			{
-				$feedback = array('success' => 0, 'message' => $sqli->connect_error);
-				echo(json_encode($feedback));
-				exit;
-			}
-			
-			//Show Chinese Chracters Correctly
-			$sqli->query("SET NAMES 'UTF8'");
-			
-			session_start();
-			$session = $_SESSION['sessionid'];
-			session_write_close();
-			
-			$result = $sqli->query("SELECT uid, name, previlege FROM user_info WHERE user_session='" . $session . "'") or die($sqli->error);
-			if (!($userinfo = $result->fetch_array(MYSQLI_ASSOC)))
-			{
-				$feedback = array('success' => 0, 'message' => 'userinfo_fetch_error');
-				echo(json_encode($feedback));
-				exit;
-			}
-			
-			$result = $sqli->query("SELECT p.p_id, p.p_name, p.p_des, p.p_company, u.name AS owner, p.p_start_time, p.p_end_time, p.status FROM project AS p LEFT JOIN user_info AS u ON p.p_owner = u.uid WHERE p_id=" . $pid . ";") or die($sqli->error);
-			if(!($project_info = $result->fetch_array(MYSQLI_ASSOC)))
-			{
-				$feedback = array('success' => 0, 'message' => 'project_fetch_error');
-				echo(json_encode($feedback));
-				exit;
-			}
-			
-			$result = $sqli->query("SELECT u.name FROM project_team AS p RIGHT JOIN user_info AS u ON u.uid = p.user_id WHERE p.project_id=" . $pid . ";") or die($sqli->error);
-			while($row = $result->fetch_array(MYSQLI_ASSOC))
-			{
-				$members[]['name'] = $row['name'];
-			}
-			
-			$reqs = array();
-			$result = $sqli->query("SELECT r.rid, r.rname, r.rtype, r.rdes, r.rstatus, r.rpriority, u.name AS owner FROM req AS r LEFT JOIN user_info AS u ON r.rowner = u.uid WHERE rproject=" . $pid . " AND rstatus != 0 ORDER BY rpriority DESC;") or die($sqli->error);
-			while($row = $result->fetch_array(MYSQLI_ASSOC))
-			{
-				$reqs[$row['rid']]['id'] = $row['rid'];
-				$reqs[$row['rid']]['name'] = $row['rname'];
-				$reqs[$row['rid']]['type'] = $row['rtype'];
-				$reqs[$row['rid']]['des'] = $row['rdes'];
-				$reqs[$row['rid']]['status'] = $row['rstatus'];
-				$reqs[$row['rid']]['priority'] = $row['rpriority'];
-				$reqs[$row['rid']]['owner'] = $row['owner'];
-			}
-		?>
 		<br>
 		<div style="z-index:1;">
 			<div class="w3-row ">
