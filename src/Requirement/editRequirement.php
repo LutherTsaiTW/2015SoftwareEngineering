26ea0ebb5e16a16c53fd1f831e3616e73cfbf21d
mickplayer <mickgamehome@gmail.com>
2015-12-02 15:54:22 +0800

    Update deleteReviewer.php Add editRequirement.php

--- src/Requirement/editRequirement.php
+++ src/Requirement/editRequirement.php
@@ -15,17 +15,20 @@ register_shutdown_function('catch_fatal_error');
 require_once '../assist/DBConfig.php';
 $database = new mysqli($dburl, $dbuser, $dbpass, $db);
 $database->query("SET NAMES 'UTF8'");
-$reviewID = $_GET['rvid'];
+$id = $_POST['id'];
+$name = $_POST['name'];
+$type = $_POST['type'];
+$description = $_POST['description'];
+$priority = $_POST['priority'];
 $result = array();
 
-$sql = "DELETE FROM `req_review` WHERE `reqreviewID` = ?";
-$stmt = $database->prepare($sql); //[MKZ] http://blog.roga.tw/2010/06/%E6%B7%BA%E8%AB%87-php-mysql-php-mysqli-pdo-%E7%9A%84%E5%B7%AE%E7%95%B0/
-$stmt->bind_param('i', $reviewID); //[MKZ] http://php.net/manual/en/mysqli-stmt.bind-param.php
+$sql = "UPDATE `req` SET `rname` = ?, `rtype` = ?, `rdes` = ?, `rpriority` = ? WHERE `rid` = ?";
+$stmt = $database->prepare($sql);
+$stmt->bind_param('sisii', $name, $type, $description, $priority, $id);
 $stmt->execute();
 
 $result['success'] = 1;
-$result['reviewID'] = $reviewID;
-$result['deleted_rows'] = $database->affected_rows;
+$result['affected_rows'] = $database->affected_rows;
 echo (json_encode($result));
 
 $stmt->close();
