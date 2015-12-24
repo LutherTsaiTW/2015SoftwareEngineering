f3e7e4007c033e07940bd4fbbaf78c8681e0ed66
andymememe <andymememe@gmail.com>
2015-12-24 13:16:39 +0800

    Project List delete edit change to being capitalized

--- src/js/showProjectList.js
+++ src/js/showProjectList.js
@@ -60,7 +60,7 @@ function insertHTML(response) {
                 else if (project[i].status == 2) out += "Terminated";
                 out += "</td> ";
                 if (project[i].owner == arr.name) {
-                    out += "<td style=\"text-align:right;\"><a href=\"editProjectView.php?pid=" + project[i].pid + "\">edit</a></td><td ><a onclick=\"showDeleteWindow(" + project[i].pid + ")\">delete</a></td></tr>";
+                    out += "<td style=\"text-align:right;\"><a href=\"editProjectView.php?pid=" + project[i].pid + "\">Edit</a></td><td ><a onclick=\"showDeleteWindow(" + project[i].pid + ")\">Delete</a></td></tr>";
                 } else {
                     out += "<td></td><td></td></tr>";
                 }
