9c26415bd17b7ef64ef293ced69d0f0f42172115
bobshih <bobshih@hotmail.com>
2015-12-28 02:28:23 +0800

    reqRelationTable.php

--- src/Report/reqRelationTable.php
+++ src/Report/reqRelationTable.php
@@ -1,10 +1,11 @@
 <?php
 	// [BC]
-	// 這個function是用來產生 requirement 和 test case 關係表格的API
+	// 這個function是用來產生 requirement 和 requirement 關係表格的API
 	// 因為是用require_once呼叫
 	// 所以必須事先準備好以下變數
 	// $pid -> 專案的id
 	// 然後會直接 echo 出表格的形式
+
 	echo "<style>
 			table, th, td {
 		    	border: 5px solid white;
@@ -15,8 +16,11 @@
 			    padding: 0 0 0 0;
 			    margin: auto;
 			}
-			td {
-				width:45px;
+			.empty{
+				background:grey;
+			}
+			th, td {
+				width: 45px;
 			}
 		</style>";
 	//echo "<body style='background:black;color:white'>";
@@ -28,7 +32,7 @@
 	$errno = mysqli_connect_errno();
 	if($errno)
 	{
-		$user = array('success' => 0, 'message' => 'db_error in reqTestcaseTable.php');
+		$user = array('success' => 0, 'message' => 'db_error in reqRelationTable.php');
 		echo(json_encode($user));
 		exit();
 	}
@@ -38,7 +42,7 @@
 	$selectReq = "SELECT rid, rnumber FROM req WHERE rproject=$pid AND (rstatus != 0 OR rstatus != 5) ORDER BY rnumber";
 	$result = $sqli->query($selectReq);
 	if(!$result){
-		echo "there is an error when SELECT requirements in reqTestcaseTable.php";
+		echo "there is an error when SELECT requirements in reqRelationTable.php";
 		exit();
 	}
 	$rpos = 0;
@@ -46,49 +50,45 @@
 		$reqs[$rpos++] = $data;
 	}
 
-	// [BC] 取得Test Cases
-	$selectTestCase = "SELECT tid, name FROM testcase WHERE pid=$pid ORDER BY tid";
-	$result = $sqli->query($selectTestCase);
-	if(!$result){
-		echo "there is an error when SELECT testcases in reqTestcaseTable.php";
-		exit();
-	}
-	$tpos = 0;
-	while ($data = $result->fetch_array()) {
-		$testcases[$tpos++] = $data;
-	}
-
 	// [BC] 開始輸出表格
 	$pos = 0;
 
 	while(true){
-		echo "<table><tr><th></th>";
-		for ($i = $pos; $i < $pos+14 && $i < $rpos ; $i++) { 
+		echo "<table><tr><th class='empty'></th>";
+		for ($i = $pos; $i < $pos+17 && $i < $rpos ; $i++) { 
 			echo "<th>R" . $reqs[$i]['rnumber'] . "</th>";
 		}
-		
+
 		echo "</tr>";
-		foreach ($testcases as $key) {
-			echo "<tr><td style='width:180px'>" . $key['name'] . "</td>";
-			for($i = $pos;$i < $pos+14 && $i < $rpos; $i++){
+		foreach ($reqs as $key) {
+			echo "<tr><td>R" . $key['rnumber'] . "</td>";
+			for($i = $pos;$i < $pos+17 && $i < $rpos; $i++){
+				// [檢查是不是一樣的 requirement]
+				if($key['rnumber'] == $reqs[$i]['rnumber']){
+					echo "<td class='empty'> </td>";
+					continue;
+				}
+
 				// [BC] 檢查 requirement 和 testcase 是否有關係
-				$getRelation = "SELECT count(relation_id) AS c FROM test_relation WHERE tid=" . $key['tid'] . " AND rid=" . $reqs[$i]['rid'];
+				$getRelation = "SELECT count(relation_id) AS c FROM req_relation WHERE (rid_a=" . $key['rid'] . " AND rid_b=" . $reqs[$i]['rid'] . ") OR (rid_a=" . $reqs[$i]['rid'] . " AND rid_b=" . $key['rid'] . ")";
 				$result = $sqli->query($getRelation);
 				if(!$result){
-					echo "there is an error when $getRelation in reqTestcaseTable.php";
+					echo "there is an error when $getRelation in reqRelationTable.php";
 					exit();
 				}
 				$temp = $result->fetch_array();
 				if($temp['c'] == 1){
 					echo "<td>O</td>";
-				} else {
+				} else if($temp['c'] == 0){
 					echo "<td> </td>";
+				} else {
+					echo "there is an error that req_relation table has two rows containing " . $key['rnumber'] . "and " . $reqs[$i]['rnumber'] . " in reqRelationTable.php";
 				}
 			}
 			echo "</tr>";
 		}
 		echo "</tr></table>";
-		$pos += 14;
+		$pos += 17;
 		if($pos < $rpos){
 			echo "<br><br>";
 		}else {
@@ -96,5 +96,6 @@
 		}
 	}
 
-	//echo "<body>";
+
+	//echo "</body>";
 ?>
