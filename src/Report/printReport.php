80ff5870a5e96600b3593ae9d21d54accd670e5c
andymememe <andymememe@gmail.com>
2015-12-29 19:32:47 +0800

    printReport.php make space between table

--- src/Report/printReport.php
+++ src/Report/printReport.php
@@ -9,22 +9,27 @@
 			if($print == 0 || $print == 1)
 			{
 				require_once('reqListTable.php');
+				echo('<br><br>');
 			}
 			if($print == 0 || $print == 2)
 			{
 				require_once('reqRelationTable.php');
+				echo('<br><br>');
 			}
 			if($print == 0 || $print == 3)
 			{
 				require_once('reqTestcaseTable.php');
+				echo('<br><br>');
 			}
 			if($print == 0 || $print == 4)
 			{
 				require_once('reqNoTestcaseTable.php');
+				echo('<br><br>');
 			}
 			if($print == 0 || $print == 5)
 			{
 				require_once('testcaseNoReqTable.php');
+				echo('<br><br>');
 			}
 		?>
 		<script>
